4d20b75d242cf362c5db2dff6036b48b2388d205
MarcelMidori <marcelmidori@users.noreply.github.com>
2026-01-16 22:23:39 +0100

    Add GitHub Pages deployment workflow and disable Jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marcelmidori.github.io',
+  base: '/home',
   integrations: [tailwind()],
 });
 
